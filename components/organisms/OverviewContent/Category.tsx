788b4e572dfaf7d4e18309eae5c16b871a0d8694
rzkzx <romanzidanramadhan@gmail.com>
2021-09-07 23:04:11 +0800

    Refactor Overview Content

---
+++ components/organisms/OverviewContent/Category.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/dist/client/image';
+import { ReactNode } from 'react';
+
+interface CategoryProps{
+    children: ReactNode,
+    nominal: number,
+    icon: 'desktop' | 'mobile';
+}
+
+export default function Category(props: CategoryProps) {
+  const { children, nominal, icon } = props;
+  return (
+    <div className="categories-card">
+      <div className="d-flex align-items-center mb-24">
+        <Image src={`/icon/ic-category/${icon}.svg`} width={60} height={60} alt=" " />
+        <p className="color-palette-1 mb-0 ms-12">
+          {children}
+        </p>
+      </div>
+      <div>
+        <p className="text-sm color-palette-2 mb-1">Total Spent</p>
+        <p className="text-2xl color-palette-1 fw-medium m-0">{nominal}</p>
+      </div>
+    </div>
+  );
+}
