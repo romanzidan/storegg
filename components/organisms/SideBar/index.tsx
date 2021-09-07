274cf8e2d36799715fd917a742242e5b8906d50f
rzkzx <romanzidanramadhan@gmail.com>
2021-09-07 23:14:42 +0800

    Sidebar Active Menu

--- components/organisms/SideBar/index.tsx
+++ components/organisms/SideBar/index.tsx
@@ -2,18 +2,23 @@ import Profile from './Profile';
 import Footer from './Footer';
 import MenuItem from './MenuItem';
 
-export default function SideBar() {
+interface SideBarProps{
+  activeMenu: 'overview' | 'transactions' | 'settings'
+}
+
+export default function SideBar(props: SideBarProps) {
+  const { activeMenu } = props;
   return (
     <section className="sidebar">
       <div className="content pt-50 pb-30 ps-30">
         <Profile />
         <div className="menus">
-          <MenuItem title="Overview" icon="overview" href="/member" active />
-          <MenuItem title="Transactions" icon="transactions" href="/member/transactions" />
-          <MenuItem title="Messages" icon="messages" href="/member/messages" />
-          <MenuItem title="Card" icon="card" href="/member/cards" />
-          <MenuItem title="Rewards" icon="rewards" href="/member/rewards" />
-          <MenuItem title="Settings" icon="settings" href="/member/edit-profile" />
+          <MenuItem title="Overview" icon="overview" href="/member" active={activeMenu === 'overview'} />
+          <MenuItem title="Transactions" icon="transactions" href="/member/transactions" active={activeMenu === 'transactions'} />
+          <MenuItem title="Messages" icon="messages" href="/member" />
+          <MenuItem title="Card" icon="card" href="/member" />
+          <MenuItem title="Rewards" icon="rewards" href="/member" />
+          <MenuItem title="Settings" icon="settings" href="/member/edit-profile" active={activeMenu === 'settings'} />
           <MenuItem title="Log Out" icon="logout" href="/logout" />
         </div>
         <Footer />
