38331dda5074dcc3237c6e1d4505ee52434893df
rzkzx <romanzidanramadhan@gmail.com>
2021-09-18 19:20:30 +0800

    Membuat configurasi API services

--- components/organisms/FeaturedGame/index.tsx
+++ components/organisms/FeaturedGame/index.tsx
@@ -1,6 +1,18 @@
+import { useCallback, useEffect, useState } from 'react';
+import { getFeaturedGame } from '../../../services/player';
 import GameItem from '../../molecules/GameItem';
 
 export default function FeaturedGame() {
+  const [gameList, setGameList] = useState([]);
+
+  const getFeatureGameList = useCallback(async () => {
+    const data = await getFeaturedGame();
+    setGameList(data);
+  }, [getFeaturedGame]);
+
+  useEffect(() => {
+    getFeatureGameList();
+  }, []);
   return (
     <section className="featured-game pt-50 pb-50">
       <div className="container-fluid">
@@ -14,11 +26,9 @@ export default function FeaturedGame() {
           className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4"
           data-aos="fade-up"
         >
-          <GameItem title="Super Mechs" category="Mobile" thumbnail="/img/Thumbnail-1.png" />
-          <GameItem title="Call of Duty: Modern" category="Mobile" thumbnail="/img/Thumbnail-2.png" />
-          <GameItem title="Valorant" category="Desktop" thumbnail="/img/Thumbnail-3.png" />
-          <GameItem title="Clash of Clans" category="Mobile" thumbnail="/img/Thumbnail-4.png" />
-          <GameItem title="Mobile Legends" category="Mobile" thumbnail="/img/Thumbnail-5.png" />
+          {gameList.map((item) => (
+            <GameItem key={item._id} title={item.name} category={item.category.name} thumbnail={`https://storegg-serv.herokuapp.com/uploads/${item.thumbnail}`} />
+          ))}
         </div>
       </div>
     </section>
