ccab3a0a3e7349c5d240a801cc2fb767d85a6ef2
rzkzx <romanzidanramadhan@gmail.com>
2021-09-20 20:33:03 +0800

    Integrasi Sign-In Page

--- components/organisms/SigninForm/index.tsx
+++ components/organisms/SigninForm/index.tsx
@@ -1,12 +1,46 @@
+import cx from 'classnames';
 import Link from 'next/link';
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import { useRouter } from 'next/router';
+
+import 'react-toastify/dist/ReactToastify.css';
+import { setLogin } from '../../../services/auth';
 
 export default function SigninForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const className = {
+    label: cx('form-label text-lg fw-medium color-palette-1 mb-10'),
+  };
+  const router = useRouter();
+
+  const onSubmit = async () => {
+    const data = {
+      email,
+      password,
+    };
+    if (!email || !password) {
+      toast.error('Email dan Password wajib diisi!!', {
+        position: 'top-left',
+        theme: 'colored',
+      });
+    } else {
+      const response = await setLogin(data);
+      if (response.error) {
+        toast.error(response.message);
+      } else {
+        toast.success('Login Berhasil');
+        router.push('/');
+      }
+    }
+  };
   return (
     <>
       <h2 className="text-4xl fw-bold color-palette-1 mb-10">Sign In</h2>
       <p className="text-lg color-palette-1 m-0">Masuk untuk melakukan proses top up</p>
       <div className="pt-50">
-        <label htmlFor="email" className="form-label text-lg fw-medium color-palette-1 mb-10">
+        <label htmlFor="email" className={className.label}>
           Email
           Address
         </label>
@@ -14,45 +48,44 @@ export default function SigninForm() {
           type="email"
           className="form-control rounded-pill text-lg"
           id="email"
-          name="email"
           aria-describedby="email"
           placeholder="Enter your email address"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
       </div>
       <div className="pt-30">
-        <label
-          htmlFor="password"
-          className="form-label text-lg fw-medium color-palette-1 mb-10"
-        >
+        <label htmlFor="password" className={className.label}>
           Password
         </label>
         <input
           type="password"
           className="form-control rounded-pill text-lg"
           id="password"
-          name="password"
           aria-describedby="password"
           placeholder="Your password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
         />
       </div>
       <div className="button-group d-flex flex-column mx-auto pt-50">
-        <a
+        <button
+          type="button"
           className="btn btn-sign-in fw-medium text-lg text-white rounded-pill mb-16"
-          href="../index.html"
-          role="button"
+          onClick={onSubmit}
         >
           Continue to Sign In
-        </a>
+        </button>
         <Link href="/sign-up">
           <a
             className="btn btn-sign-up fw-medium text-lg color-palette-1 rounded-pill"
-            role="button"
           >
             Sign
             Up
           </a>
         </Link>
       </div>
+      <ToastContainer />
     </>
   );
 }
