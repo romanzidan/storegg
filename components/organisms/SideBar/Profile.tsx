035a8fee22a12cd4573f43df8ffa72ba2223c14f
rzkzx <romanzidanramadhan@gmail.com>
2021-09-23 23:42:01 +0800

    Integrasi Sidebar Profile Member

--- components/organisms/SideBar/Profile.tsx
+++ components/organisms/SideBar/Profile.tsx
@@ -1,9 +1,31 @@
+import Cookies from 'js-cookie';
+import jwtDecode from 'jwt-decode';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { JWTPayloadTypes, UserTypes } from '../../../services/data-types';
+
 export default function Profile() {
+  const imgAPI = process.env.NEXT_PUBLIC_IMG;
+  const [user, setUser] = useState({
+    name: '',
+    avatar: '',
+    email: '',
+  });
+  useEffect(() => {
+    const token = Cookies.get('token');
+    if (token) {
+      const jwtToken = atob(token);
+      const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+      const userFromPayload: UserTypes = payload.player;
+      setUser(userFromPayload);
+      console.log(userFromPayload);
+    }
+  }, []);
   return (
     <div className="user text-center pb-50 pe-30">
-      <img src="/img/avatar-1.png" width={90} height={90} alt="avatar" className="img-fluid mb-20" />
-      <h2 className="fw-bold text-xl color-palette-1 m-0">Shayna Anne</h2>
-      <p className="color-palette-2 m-0">shayna@anne.com</p>
+      <Image src={`${imgAPI}/${user.avatar}`} width={90} height={90} alt="avatar" className="img-fluid mb-20 rounded-circle" />
+      <h2 className="fw-bold text-xl color-palette-1 m-0">{user.name}</h2>
+      <p className="color-palette-2 m-0">{user.email}</p>
     </div>
   );
 }
