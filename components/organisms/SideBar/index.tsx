020b3b4fcc2e7805d14b18612b2d83427a1371c3
rzkzx <romanzidanramadhan@gmail.com>
2021-09-24 11:02:50 +0800

    Logout Member Page

--- components/organisms/SideBar/index.tsx
+++ components/organisms/SideBar/index.tsx
@@ -1,3 +1,7 @@
+import Image from 'next/image';
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/router';
+import { toast } from 'react-toastify';
 import Profile from './Profile';
 import Footer from './Footer';
 import MenuItem from './MenuItem';
@@ -8,6 +12,19 @@ interface SideBarProps{
 
 export default function SideBar(props: SideBarProps) {
   const { activeMenu } = props;
+  const router = useRouter();
+
+  const onLogout = () => {
+    Cookies.remove('token');
+    toast.success('Logout Berhasil !!!', {
+      position: 'top-center',
+      theme: 'colored',
+      pauseOnHover: false,
+      autoClose: 3000,
+    });
+    router.push('/sign-in');
+  };
+
   return (
     <section className="sidebar">
       <div className="content pt-50 pb-30 ps-30">
@@ -19,7 +36,20 @@ export default function SideBar(props: SideBarProps) {
           <MenuItem title="Card" icon="card" href="/member" />
           <MenuItem title="Rewards" icon="rewards" href="/member" />
           <MenuItem title="Settings" icon="settings" href="/member/edit-profile" active={activeMenu === 'settings'} />
-          <MenuItem title="Log Out" icon="logout" href="/logout" />
+          <div className="item mb-30">
+            <div className="me-3">
+              <Image src="/icon/ic-menu/logout.svg" width={25} height={25} alt="Overview" />
+            </div>
+            <p className="item-title m-0">
+              <button
+                type="button"
+                className="text-lg text-decoration-none"
+                onClick={onLogout}
+              >
+                Logout
+              </button>
+            </p>
+          </div>
         </div>
         <Footer />
       </div>
