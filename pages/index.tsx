c81d4b38305254f1c046a4c9bda1b0a09a0c6802
rzkzx <romanzidanramadhan@gmail.com>
2021-09-27 19:03:27 +0800

    Setup Basic SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import AOS from 'aos';
+import Head from 'next/head';
 import Navbar from '../components/organisms/Navbar';
 import MainBanner from '../components/organisms/MainBanner';
 import TransactionStep from '../components/organisms/TransactionStep';
@@ -14,6 +15,16 @@ export default function Home() {
   }, []);
   return (
     <>
+      <Head>
+        <title>StoreGG - Get a New Experiene in Gaming</title>
+        <link rel="shortcut icon" href="icon/logo.svg" type="image/x-icon" />
+        <meta name="description" content="Kami menyediakan jutaan cara untuk menjadi gamer terbaik" />
+        <meta property="og:title" content="StoreGG - Get a New Experiene in Gaming" />
+        <meta property="og:description" content="Kami menyediakan jutaan cara untuk menjadi gamer terbaik" />
+        <meta property="og:image" content="https://storegg-serv.herokuapp.com/uploads/5.png" />
+        <meta property="og:image" content="https://storegg.com" />
+      </Head>
+
       <Navbar />
       <MainBanner />
       <TransactionStep />
